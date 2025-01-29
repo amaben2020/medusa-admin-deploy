7e41b15f6d4a77a16fe7908e6946d117a59ac166
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-01-29 09:14:54 +0100

    build update

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,25 +1,10 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils';
+import { loadEnv, defineConfig, Modules } from '@medusajs/framework/utils';
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd());
-
-const plugins = [
-  {
-    resolve: `medusa-payment-stripe`,
-    options: {
-      api_key: process.env.STRIPE_API_KEY,
-      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-    },
-  },
-  {
-    resolve: 'medusa-plugin-wishlist',
-  },
-];
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    // databaseUrl:
-    //   'postgresql://neondb_owner:npg_F82ukohdXraz@ep-nameless-mountain-a40whqli-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require',
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -27,7 +12,16 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || 'supersecret',
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
     },
+    workerMode: process.env.MEDUSA_WORKER_MODE as
+      | 'shared'
+      | 'worker'
+      | 'server',
+    redisUrl: process.env.REDIS_URL,
   },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === 'true',
+  },
+
   plugins: [
     {
       resolve: '@medusajs/payment',
@@ -49,6 +43,29 @@ module.exports = defineConfig({
             public_key: process.env.PAYSTACK_PUBLIC_KEY,
           },
         ],
+      },
+    },
+  ],
+
+  modules: [
+    {
+      resolve: '@medusajs/medusa/cache-redis',
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: '@medusajs/medusa/event-bus-redis',
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: '@medusajs/medusa/workflow-engine-redis',
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
       },
     },
   ],
